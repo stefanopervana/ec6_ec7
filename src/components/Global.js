4bfdb0658fddc34c55867ae226604532a21674f4
stefanopervana <estebanpervanas@gmail.com>
2017-07-13 22:07:18 -0300

    Add files via upload

---
+++ src/components/Global.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
+class Global extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			query: ''
+		}
+	}
+
+	search() {
+		const BASE_URL = 'http://myhost.org/cgi-bin/dbbindings.py?queryTerms={"database":"YOURDBNAME","method":"returnPossibleFields"}';
+		fetch(`${BASE_URL}${this.state.query}`, { method: 'GET'} )
+			.then(response => response.json())
+			.then(json => console.log(json));
+	}
+
+	render(){
+		return (
+			<div className="Global">
+			<h2>Book Explorer!</h2>
+			<FormGroup>
+				<InputGroup>
+					<FormControl
+					type="text"
+						placeholder="Search for a book"
+						onChange={event => this.setState({query: event.target.value})}
+						onKeyPress={event => {
+							if (event.key === 'Enter') {
+								this.search();
+							}
+						}}
+						/>
+						<InputGroup.Addon onClick={() => this.search()}>
+							<Glyphicon glyph="search"></Glyphicon>
+						</InputGroup.Addon>
+				</InputGroup>
+			</FormGroup>
+			</div>
+		) 
+	}
+}
+
+export default Global;
